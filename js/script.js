e09c9dd6316c1dc87db5c75f3708b8397ad8b14b
KuroNoDev <kuronogiasu@gmail.com>
2022-06-21 21:32:00 +0900

    update UI

--- js/script.js
+++ js/script.js
@@ -56,9 +56,9 @@ monogatari.configuration ('credits', {
       <a target='_blank' href='https://twitter.com/questionablitiy'>Questia クエスティア</a><br>
       <a target='_blank' href='https://twitter.com/DGBCH11'>Colin The Ryuguard</a><br>
       <a target='_blank' href='https://twitter.com/NijiEnTako'>Tako</a>`,
-    "Misc": `Finana Finclub Discord Server, OLcord and <a target='_blank' href='https://twitter.com/AvrosKyargath'>Avros</a>`
+    "Misc": `Finana Finclub Discord Server and OLcord`
 	},
-	"Special Thanks to our <a target='_blank' href='https://twitter.com/FinanaRyugu'> KAMI OSHI</a> You have changed our lives more than you'll ever know and have inspired all of us to be a better person each day. WE LOVE YOU AND HAPPY BIRTHDAY!!!!" : {}
+	"Special Thanks to our <a target='_blank' href='https://twitter.com/FinanaRyugu'>KAMI OSHI</a><br>You have changed our lives more than you'll ever know and have inspired all of us to be a better person each day<br>WE LOVE YOU AND HAPPY BIRTHDAY!!!!" : {}
 });
 
 
